6e22c61c2c022d9eeb6707597c815c592064ff74
KayMohh <muhammadkadir022@gmail.com>
2025-02-01 00:03:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,33 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my HNG Api"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get('/hng')
+def get_data():
+    email = 'thrilltim@gmail.com'
+    current_datetime =  datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    github_repo_url = "https://github.com/kaymohh/HNG"
+
+    current_day = datetime.now().strftime("%A")
+
+    return {
+        'email' : email,
+        'current_datetime' : current_datetime,
+        'github_repo_url' : github_repo_url,
+        "status_code": 200,
+
+    }
